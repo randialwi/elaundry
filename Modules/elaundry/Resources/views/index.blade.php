cb4c92197cc82473082f9aa696999a62ff5a3c5e
randialwi <mrandialwi@gmail.com>
2020-04-03 16:51:27 +0700

    1
    1

---
+++ Modules/elaundry/Resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('elaundry::layouts.app')
+@section('content')
+ <div class="container-fluid">
+     <div class="row">
+         <div class="col-sm-12">
+             <div class="card">
+                 <div class="card-header">
+                    <b>Home</b> | welcome
+                 </div>
+                 <div class="card-body">
+                     <div class="container">
+                     </div>
+                 </div>
+                 <div class="card-footer">
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+@endsection
---
+++ Modules/elaundry/Resources/views/layouts/user-menunav.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-sm navbar-light">
+    <ul class="navbar-nav ">
+        <li class="nav-item bg-primary active">
+            <a class="nav-link text-black" href="{{ url('/elaundry') }}">Home</a>
+        </li>
+    </ul>
+</nav>
